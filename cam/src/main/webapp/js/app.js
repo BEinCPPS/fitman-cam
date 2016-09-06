96745042202a6410709f25dd2e9793d751a81a12
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2016-09-06 10:38:01 +0200

    Update app.js

--- cam/src/main/webapp/js/app.js
+++ cam/src/main/webapp/js/app.js
@@ -5,7 +5,7 @@ camApp.config(['ngDialogProvider', function (ngDialogProvider) {
     ngDialogProvider.setDefaults({
         plain: false,
         showClose: false,
-        closeByDocument: true,
+        closeByDocument: false,
         closeByEscape: true
     });
  }]);
