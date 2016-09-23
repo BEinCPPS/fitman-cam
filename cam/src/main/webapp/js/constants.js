5400f4f8936c55f51e269eb7d1d64417c8797f39
ascatox <antonio.scatoloni@gmail.com>
2016-09-23 17:11:56 +0200

    Owners 1st.

--- cam/src/main/webapp/js/constants.js
+++ cam/src/main/webapp/js/constants.js
@@ -1,7 +1,6 @@
-//var config = $http.get('resources/config.json');    
 var BACK_END_URL_CONST="http://"+window.location.hostname+":8080/CAMService";
 var INVALID_NAME_MSG="Please insert valid name";
 var NAME_IS_MANDATORY_MSG="Name is mandatory";
 var REGEX_PATTERN="^d*[A-Za-z0-9_@./+-]*$";
 var HTTP_TIMEOUT = 20000; //expressed in milliseconds
-var HTTP_TIMEOUT_EXPIRED_MSG="System not available at the moment :-(\nPlease try later!";
+var HTTP_TIMEOUT_EXPIRED_MSG="System not available at the moment!\nPlease try later!";
