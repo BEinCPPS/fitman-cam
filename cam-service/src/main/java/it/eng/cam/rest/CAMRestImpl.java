df5d06797305af8bb4428e92263220d99a99552a
ascatox <antonio.scatoloni@gmail.com>
2017-01-17 16:24:24 +0100

    Update domain and other improvements.

--- cam-service/src/main/java/it/eng/cam/rest/CAMRestImpl.java
+++ cam-service/src/main/java/it/eng/cam/rest/CAMRestImpl.java
@@ -389,7 +389,7 @@ public class CAMRestImpl {
 
     public static void deleteOrionConfig(RepositoryDAO dao, String orionConfigId) {
         List<IndividualItem> individualsByOrionConfig = dao.getIndividualsByOrionConfig(orionConfigId);
-        if (individualsByOrionConfig.size() > 0) {
+        if (null != individualsByOrionConfig && individualsByOrionConfig.size() > 0) {
             throw new IllegalStateException("Orion Configuration cannot be deleted as it is referenced somewhere else");
         }
         dao = releaseRepo(dao);
